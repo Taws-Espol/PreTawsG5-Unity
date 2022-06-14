1af11432e7521e0999efb786af8689e6a66e8b79
Your Name <camdorey@espol.edu.ec>
2022-06-13 22:28:25 -0500

    Fondo terminado
    Añadi unos obstaculos y monedas

--- Assets/Scripts/Eliminar_objetos.cs
+++ Assets/Scripts/Eliminar_objetos.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Eliminar_objetos : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("choca");
         if (other.GetComponent<Barrera>() != null)
