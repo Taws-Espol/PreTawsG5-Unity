32ada4a513e5c008cf5eafbad1a5d3fd2db2f40d
Joshua Cobos <44756521+LIMUNQUE@users.noreply.github.com>
2022-07-10 20:30:53 -0500

    pong level

---
+++ Assets/Scripts/Pong/Players.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Players : MonoBehaviour
+{
+    public bool player1;
+    public float speed = 200f;
+    public Rigidbody2D rb;
+
+    private float move;
+    public Vector2 startPos;
+
+    void Start(){
+        startPos = transform.position;
+    }
+
+    void Update(){
+        if(player1){
+            move = Input.GetAxis("Vertical1");
+        }
+        else{
+            move = Input.GetAxis("Vertical2");
+        }
+        rb.velocity = new Vector2(0, move * speed * Time.deltaTime);
+    }
+    public void Reset(){
+        rb.velocity = Vector2.zero;
+        transform.position = startPos;
+    }
+}
+
