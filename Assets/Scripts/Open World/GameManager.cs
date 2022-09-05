1782120769de93a440f2533e7d393bef5ef52f29
Joshua Cobos <44756521+LIMUNQUE@users.noreply.github.com>
2022-09-04 20:22:08 -0500

    Charaters

--- Assets/Scripts/Open World/GameManager.cs
+++ Assets/Scripts/Open World/GameManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.IO;
 
 public class GameManager : MonoBehaviour
 {
@@ -33,6 +35,32 @@ public class GameManager : MonoBehaviour
         instance.audioSource.Play();
     }
 
+    [Serializable]
+    class BestScorePong{
+        public int score;
+        public string name;
+    }
 
-    
+    public void SaveScorePong(){
+        BestScorePong data = new BestScorePong();
+        data.name = bestNombrePong;
+        data.score = bestPointsPong;
+
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText(Application.persistentDataPath + "/saveFilePong.json", json);
+    }
+
+    public void LoadScorePong(){
+        string path = Application.persistentDataPath + "/saveFilePong.json";
+        if(File.Exists(path)){
+            string json = File.ReadAllText(path);
+            BestScorePong data = JsonUtility.FromJson<BestScorePong>(json);
+            bestNombrePong = data.name;
+            bestPointsPong = data.score;
+        }
+        else{
+            bestNombrePong = "NA";
+            bestPointsPong = 0;
+        }
+    }
 }
