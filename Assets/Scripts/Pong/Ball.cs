f7d5f1051476e8811a6b0d93ae1ac79c307fd0ba
Joshua Cobos <44756521+LIMUNQUE@users.noreply.github.com>
2022-09-06 23:45:34 -0500

    Cambio de escenas
    Menu entre juegos

--- Assets/Scripts/Pong/Ball.cs
+++ Assets/Scripts/Pong/Ball.cs
@@ -2,14 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-
+using UnityEngine.SceneManagement;
 public class Ball : MonoBehaviour
 {
-    public float speed=100f;
+    public float speed=360;
     public Rigidbody2D rb;
     public Vector2 startPos;
     [SerializeField] private AudioClip[] ballSounds;
     private AudioSource audioSource;
+
+    public GameObject panel;
+    [SerializeField] private TMP_Text puntos;
+    [SerializeField] private TMP_Text BestScore;
 
     [SerializeField] private TMP_Text goalLeft;
     [SerializeField] private TMP_Text goalRight;
@@ -17,6 +21,7 @@ public class Ball : MonoBehaviour
     public GameObject playerRight;
     int scoreLeft = 0;
     int scoreRight = 0;
+
 
     public bool infiniteMode=false;
     [SerializeField] private TMP_Text timer;
@@ -28,18 +33,21 @@ public class Ball : MonoBehaviour
         transform.position = startPos;
         audioSource = GetComponent<AudioSource>();
         audioSource.clip = ballSounds[0];
+        
         Launch();
     }
 
     public void Launch(){
+        panel.SetActive(false);
         float x = Random.Range(0, 2) == 0 ? -1 : 1;
         float y = Random.Range(0, 2) == 0 ? -1 : 1;
 
-        rb.velocity = new Vector2(x, y) * speed*Time.deltaTime;
+        rb.velocity = new Vector2(x, y) * speed*Time.deltaTime*2;
     }
     public void Reset(){
         if(!IAGame){
-            playerRight.GetComponent<Players>().Reset();   
+            playerRight.GetComponent<Players>().Reset();
+            Launch();   
         }
         playerLeft.GetComponent<Players>().Reset();
         rb.velocity = Vector2.zero;
@@ -58,6 +66,7 @@ public class Ball : MonoBehaviour
             goalLeft.text = "Lose!";
             goalRight.text = "Win!";
             Time.timeScale = 0;
+
         }
         
     }
@@ -87,6 +96,17 @@ public class Ball : MonoBehaviour
                     goalLeft.text = scoreLeft.ToString();
                     goalRight.text = "End";
                     Time.timeScale = 0;
+                    panel.SetActive(true);
+                    puntos.text = "Score: " + scoreLeft.ToString();
+                    
+
+                    if(scoreLeft > GameManager.instance.bestPointsPong){
+                        GameManager.instance.bestPointsPong = scoreLeft;
+                        GameManager.instance.bestNombrePong = "anonymous";
+                        GameManager.instance.SaveScorePong();
+                        
+                    }
+                    BestScore.text = $"Best Score: {GameManager.instance.bestPointsPong} - {GameManager.instance.bestNombrePong}";
                 }
             }
         }
@@ -98,4 +118,15 @@ public class Ball : MonoBehaviour
             timer.text = timeText.ToString("f0");
         }
     }
+
+    public void btnJugar(){
+        Time.timeScale = 1;
+        goalRight.text = "0";
+        goalLeft.text = "0";
+        scoreLeft = 0;
+        scoreRight = 0;
+        timeText = 0;
+        Reset();
+    }
+
 }
